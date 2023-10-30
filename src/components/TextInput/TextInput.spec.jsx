57f21e084b78e5d7a8396c13ecaf945291db49d3
DiogoBlanco <diogoblanco@outlook.com.br>
2023-10-30 11:08:21 -0300

    tests

---
+++ src/components/TextInput/TextInput.spec.jsx
@@ -0,0 +1,23 @@
+import userEvent from "@testing-library/user-event";
+import { TextInput } from ".";
+
+import { render, screen } from "@testing-library/react";
+
+describe("<TextInput/>", () => {
+  it("should have a value of searchValue", () => {
+    const fn = jest.fn();
+    render(<TextInput handleChange={fn} searchValue={"testando"} />);
+    const input = screen.getByPlaceholderText(/type your search/i);
+    expect(input.value).toBe("testando");
+  });
+
+  it("should call handleChange function on each key press", () => {
+    const fn = jest.fn();
+    render(<TextInput handleChange={fn} />);
+    const input = screen.getByPlaceholderText(/type your search/i);
+    const value = "o valor";
+    userEvent.type(input, value);
+    expect(input.value).toBe(value);
+    expect(fn).toHaveBeenCalledTimes(value.length);
+  });
+});
