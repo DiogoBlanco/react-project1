eadd5567a4e3e0597e4cf469476eae9bc026efab
DiogoBlanco <diogoblanco@outlook.com.br>
2023-10-31 18:34:03 -0300

    tests

--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { Home } from '.';
+import { waitForElementToBeRemoved, render, screen } from '@testing-library/react';
+
+const handlers = [
+  rest.get('https://jsonplaceholder.typicode.com/posts', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title1',
+          body: 'body1',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title2',
+          body: 'body2',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title3',
+          body: 'body3',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
 
 describe('<Home/>', () => {
-  it('test one', () => {
-    expect(1).toBe(1);
+  beforeAll(() => {
+    server.listen();
+  });
+  afterEach(() => {
+    server.resetHandlers();
+  });
+  afterAll(() => {
+    server.close();
+  });
+  it('should render search, posts and load more', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts');
+    await waitForElementToBeRemoved(noMorePosts);
   });
 });
